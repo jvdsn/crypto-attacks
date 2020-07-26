c894dab9861f1c39f2e7847fba9f79f4e68e7ed0
Joachim Vandersmissen <git@jvdsn.com>
2020-07-26 17:35:04 +0200

    Small changes to the ElGamal signature nonce reuse attack signature.

--- elgamal_signature/nonce_reuse.py
+++ elgamal_signature/nonce_reuse.py
@@ -2,7 +2,7 @@ from math import gcd
 
 
 # Solves a congruence of the form ax = b mod n
-def solve_congruence(a, b, n):
+def solve_congruence_(a, b, n):
     g = gcd(a, n)
     a //= g
     b //= g
@@ -11,26 +11,18 @@ def solve_congruence(a, b, n):
         yield (pow(a, -1, m) * b + i * m) % n
 
 
-def attack(p, g, y, m1, r1, s1, m2, r2, s2):
+def attack(m1, r1, s1, m2, r2, s2, p):
     """
-    Recovers the private key from two messages signed using the same nonce.
-    :param p: the prime used in the ElGamal scheme
-    :param g: the generator used in the ElGamal scheme
-    :param y: the public key
+    Recovers the nonce and private key from two messages signed using the same nonce.
     :param m1: the first message
     :param r1: the signature of the first message
     :param s1: the signature of the first message
     :param m2: the second message
     :param r2: the signature of the second message
     :param s2: the signature of the second message
-    :return: the private key
+    :param p: the prime used in the ElGamal scheme
+    :return: tuples containing the possible nonce and private key
     """
-    for k in solve_congruence(s1 - s2, m1 - m2, p - 1):
-        if pow(g, k, p) == r1:
-            break
-
-    for x in solve_congruence(r1, m1 - k * s1, p - 1):
-        if pow(g, x, p) == y:
-            return x
-
-    return None
+    for k in solve_congruence_(s1 - s2, m1 - m2, p - 1):
+        for x in solve_congruence_(r1, m1 - k * s1, p - 1):
+            yield k, x
