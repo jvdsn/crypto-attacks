be34d3919d46941b7633c8fd963148ef9c5bad7d
Joachim Vandersmissen <git@jvdsn.com>
2020-08-18 21:41:27 +0200

    Fixed Bleichenbacher's signature forgery attack.

--- rsa/bleichenbacher_signature_forgery.py
+++ rsa/bleichenbacher_signature_forgery.py
@@ -1,17 +1,13 @@
-def attack(suffix):
+def attack(suffix_bits, suffix):
     """
     Returns a number s for which s^3 ends with the provided suffix.
+    :param suffix_bits: the amount of bits in the suffix
     :param suffix: the suffix
     :return: the number s
     """
     s = 1
-    c = 1
-    i = 0
-    while (1 << i) <= suffix:
-        if ((c >> i) & 1) != ((suffix >> i) & 1):
-            s ^= (1 << i)
-            c = s ** 3
-
-        i += 1
+    for i in range(suffix_bits):
+        if (((s ** 3) >> i) & 1) != ((suffix >> i) & 1):
+            s |= (1 << i)
 
     return s
