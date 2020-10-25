491ce1d7005dcef6e1e44ee0e81a845832526bea
Joachim Vandersmissen <git@jvdsn.com>
2020-10-25 19:12:39 +0100

    Made GCD algorithm more general and robust, clarified attack description

--- rsa/related_message.py
+++ rsa/related_message.py
@@ -2,11 +2,17 @@ from sage.all import PolynomialRing
 from sage.all import Zmod
 
 
+# Euclid's algorithm for polynomials
 def _polynomial_gcd(a, b):
-    while b:
-        a, b = b, a % b
+    assert a.base_ring() == b.base_ring()
 
-    return a.monic()
+    while b:
+        try:
+            a, b = b, a % b
+        except RuntimeError:
+            raise ArithmeticError("b is not invertible", b)
+
+    return a
 
 
 def attack(n, e, c1, c2, f1, f2):
@@ -16,13 +22,13 @@ def attack(n, e, c1, c2, f1, f2):
     :param e: the public exponent
     :param c1: the ciphertext of the first encryption
     :param c2: the ciphertext of the second encryption
-    :param f1: the polynomial encoding the shared secret into the first plaintext
-    :param f2: the polynomial encoding the shared secret into the second plaintext
+    :param f1: the function encoding the shared secret into the first plaintext
+    :param f2: the function encoding the shared secret into the second plaintext
     :return: the shared secret
     """
     pr = PolynomialRing(Zmod(n), "x")
     x = pr.gen()
     g1 = f1(x) ** e - c1
     g2 = f2(x) ** e - c2
-    g = -_polynomial_gcd(g1, g2)
+    g = -_polynomial_gcd(g1, g2).monic()
     return g[0]
