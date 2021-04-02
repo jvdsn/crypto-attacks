af2e8e6095b8771f89bc658d7e294241858f578e
Joachim Vandersmissen <git@jvdsn.com>
2021-04-02 23:48:01 +0200

    Use small capital matrix from sage.

--- ecc/parameter_recovery.py
+++ ecc/parameter_recovery.py
@@ -1,5 +1,5 @@
 from sage.all import GF
-from sage.all import Matrix
+from sage.all import matrix
 from sage.all import vector
 
 
@@ -13,7 +13,7 @@ def attack(p, x1, y1, x2, y2):
     :param y2: the y coordinate of the second point
     :return: a tuple containing the a and b parameters of the elliptic curve
     """
-    m = Matrix(GF(p), [[x1, 1], [x2, 1]])
+    m = matrix(GF(p), [[x1, 1], [x2, 1]])
     v = vector(GF(p), [y1 ** 2 - x1 ** 3, y2 ** 2 - x2 ** 3])
     a, b = m.solve_right(v)
     return int(a), int(b)
