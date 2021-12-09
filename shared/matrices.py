936a96d02155c0ff0a01b5697275082ddbea9ea3
Joachim Vandersmissen <git@jvdsn.com>
2021-12-09 23:29:10 +0100

    Updated matrix dlog to work in extension fields (somewhat).

--- shared/matrices.py
+++ shared/matrices.py
@@ -1,29 +1,61 @@
+import logging
+
+from sage.all import identity_matrix, GF
+from sage.matrix.matrix2 import _jordan_form_vector_in_difference
+
+
+def find_eigenvalues(A):
+    """
+    Computes the eigenvalues and P matrices for a specific matrix A.
+    :param A: the matrix A.
+    :return: a generator generating tuples of
+        K: the extension field of the eigenvalue,
+        k: the degree of the factor of the charpoly associated with the eigenvalue,
+        e: the multiplicity of the factor of the charpoly associated with the eigenvalue,
+        l: the eigenvalue,
+        P: the transformation matrix P (only the first e columns are filled)
+    """
+    factors = {}
+    for g, e in A.charpoly().factor():
+        k = g.degree()
+        if k not in factors or e > factors[k][0]:
+            factors[k] = (e, g)
+
+    p = A.base_ring().order()
+    for k, (e, g) in factors.items():
+        logging.debug(f"Found factor {g} with degree {k} and multiplicity {e}")
+        K = GF(p ** k, "x", modulus=g, impl="modn" if k == 1 else "pari")
+        l = K.gen()
+        # Assuming there is only 1 Jordan block for this eigenvalue.
+        Vlarge = ((A - l) ** e).right_kernel().basis()
+        Vsmall = ((A - l) ** (e - 1)).right_kernel().basis()
+        v = _jordan_form_vector_in_difference(Vlarge, Vsmall)
+        P = identity_matrix(K, A.nrows())
+        for i in reversed(range(e)):
+            P.set_row(i, v)
+            v = (A - l) * v
+
+        P = P.transpose()
+        yield K, k, e, l, P
+
+
 def dlog(A, B):
     """
-    Computes l such that A^l = B, in GF(p).
+    Computes l such that A^l = B.
     :param A: the matrix A
     :param B: the matrix B
-    :return: l, or None if l could not be found
+    :return: a generator generating values for l and m, where A^l = B mod m.
     """
     assert A.is_square() and B.is_square() and A.nrows() == B.nrows()
 
-    # TODO: extend to GF(p^k) if necessary?
-    J, P = A.jordan_form(transformation=True)
-    B = P ** -1 * B * P
-    r = 0
-    for s1, s2 in zip(*J.subdivisions()):
-        S = J.subdivision(s1, s2)
-        assert S.is_square()
-
-        n = S.nrows()
-        r += n
-        if n >= 2:
-            B1 = B[r - 1, r - 1]
-            B2 = B[r - 2, r - 1]
-            l = (S[n - 1, n - 1] * B2) / B1
-            return int(l)
-
-    return None
+    for K, k, e, l, P in find_eigenvalues(A):
+        B_ = P ** -1 * B * P
+        logging.debug(f"Computing dlog in {K}...")
+        yield int(B_[0, 0].log(l)), int(p ** k - 1)
+        if e >= 2:
+            B1 = B_[e - 1, e - 1]
+            B2 = B_[e - 2, e - 1]
+            yield int((l * B2) / B1), int(p ** k)
 
 
 def dlog_equation(A, x, y):
