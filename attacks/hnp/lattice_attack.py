6aed642a1196c3ddb89b416ff202385d95177762
Joachim Vandersmissen <git@jvdsn.com>
2022-01-06 22:53:50 +0100

    Use PartialIntegers for DSA lattice attacks.

--- attacks/hnp/lattice_attack.py
+++ attacks/hnp/lattice_attack.py
@@ -47,78 +47,80 @@ def attack(a, b, m, X):
             yield xs, ys
 
 
-def dsa_known_msb(n, signatures, nonce_bitsize, msb_known):
+def dsa_known_msb(n, signatures, partial_nonces):
     """
     Recovers the (EC)DSA private key and nonces if the most significant nonce bits are known.
     :param n: the modulus
-    :param signatures: a list containing the signatures (a tuple of the message (hash), the r value, the s value, and the known msbs)
-    :param nonce_bitsize: the amount of bits of the nonces
-    :param msb_known: the amount of known most significant bits of the nonces
+    :param signatures: a list containing the signatures (a tuple of the message (hash), the r value, the s value)
+    :param partial_nonces: a list containing the partial nonces (PartialIntegers)
     :return: a generator generating tuples containing the possible private key and a list of nonces
     """
     a = []
     b = []
-    X = 2 ** (nonce_bitsize - msb_known)
-    shift = 2 ** (nonce_bitsize - msb_known)
-    for h, r, s, msb in signatures:
+    X = 0
+    for (h, r, s), partial_nonce in zip(signatures, partial_nonces):
+        msb, msb_bit_length = partial_nonce.get_known_msb()
+        shift = 2 ** partial_nonce.get_unknown_lsb()
         a.append([pow(s, -1, n) * r])
         b.append(pow(s, -1, n) * h - shift * msb)
+        X = max(X, shift)
 
-    for nonces, private_key in attack(a, b, n, X):
-        for i in range(len(nonces)):
-            # Adding the MSB back to the nonce.
-            nonces[i] = int(shift * signatures[i][3] + nonces[i])
-
+    for nonce_lsbs, private_key in attack(a, b, n, X):
+        nonces = [partial_nonce.sub([lsb]) for partial_nonce, lsb in zip(partial_nonces, nonce_lsbs)]
         yield private_key[0], nonces
 
 
-def dsa_known_lsb(n, signatures, nonce_bitsize, lsb_known):
+def dsa_known_lsb(n, signatures, partial_nonces):
     """
     Recovers the (EC)DSA private key and nonces if the least significant nonce bits are known.
     :param n: the modulus
     :param signatures: a list containing the signatures (a tuple of the message (hash), the r value, the s value, and the known lsbs)
-    :param nonce_bitsize: the amount of bits of the nonces
-    :param lsb_known: the amount of known least significant bits of the nonces
+    :param partial_nonces: a list containing the partial nonces (PartialIntegers)
     :return: a generator generating tuples containing the possible private key and a list of nonces
     """
     a = []
     b = []
-    X = 2 ** (nonce_bitsize - lsb_known)
-    shift = 2 ** lsb_known
-    invshift = pow(shift, -1, n)
-    for h, r, s, lsb in signatures:
+    X = 0
+    for (h, r, s), partial_nonce in zip(signatures, partial_nonces):
+        lsb, lsb_bit_length = partial_nonce.get_known_lsb()
+        invshift = pow(2 ** lsb_bit_length, -1, n)
         a.append([invshift * pow(s, -1, n) * r])
         b.append(invshift * pow(s, -1, n) * h - invshift * lsb)
+        X = max(X, 2 ** partial_nonce.get_unknown_msb())
 
-    for nonces, private_key in attack(a, b, n, X):
-        for i in range(len(nonces)):
-            # Adding the LSB back to the nonce.
-            nonces[i] = int(shift * nonces[i] + signatures[i][3])
-
+    for nonce_msbs, private_key in attack(a, b, n, X):
+        nonces = [partial_nonce.sub([msb]) for partial_nonce, msb in zip(partial_nonces, nonce_msbs)]
         yield private_key[0], nonces
 
 
-def dsa_known_middle(n, signature1, signature2, nonce_bitsize, msb_unknown, lsb_unknown):
+def dsa_known_middle(n, signature1, partial_nonce1, signature2, partial_nonce2):
     """
     Recovers the (EC)DSA private key and nonces if the middle nonce bits are known.
     This is a heuristic extension which might perform worse than the methods to solve the Extended Hidden Number Problem.
     More information: De Micheli G., Heninger N., "Recovering cryptographic keys from partial information, by example" (Section 5.2.3)
     :param n: the modulus
-    :param signature1: the first signature (a tuple of the message (hash), the r value, the s value, and the known middle bits)
-    :param signature2: the second signature (a tuple of the message (hash), the r value, the s value, and the known middle bits)
-    :param nonce_bitsize: the amount of bits of the nonces
-    :param msb_unknown: the amount of unknown most significant bits of the nonces
-    :param lsb_unknown: the amount of unknown least significant bits of the nonces
+    :param signature1: the first signature (a tuple of the message (hash), the r value, the s value)
+    :param partial_nonce1: the first partial nonce (PartialInteger)
+    :param signature2: the second signature (a tuple of the message (hash), the r value, the s value)
+    :param partial_nonce2: the second partial nonce (PartialInteger)
     :return: a tuple containing the private key, the nonce of the first signature, and the nonce of the second signature
     """
-    K = 2 ** max(msb_unknown, lsb_unknown)
-    l = nonce_bitsize - msb_unknown
+    nonce_bit_length = partial_nonce1.bit_length
+    assert nonce_bit_length == partial_nonce2.bit_length
+    lsb_bit_length = partial_nonce1.get_unknown_lsb()
+    assert lsb_bit_length == partial_nonce2.get_unknown_lsb()
+    msb_bit_length = partial_nonce1.get_unknown_msb()
+    assert msb_bit_length == partial_nonce2.get_unknown_msb()
+    K = 2 ** max(lsb_bit_length, msb_bit_length)
+    l = nonce_bit_length - msb_bit_length
 
-    h1, r1, s1, a1 = signature1
-    h2, r2, s2, a2 = signature2
+    h1, r1, s1 = signature1
+    h2, r2, s2 = signature2
+    a1 = partial_nonce1.get_shifted_value(1)
+    a2 = partial_nonce2.get_shifted_value(1)
     t = -(pow(s1, -1, n) * s2 * r1 * pow(r2, -1, n))
     u = pow(s1, -1, n) * r1 * h2 * pow(r2, -1, n) - pow(s1, -1, n) * h1
-    u_ = 2 ** lsb_unknown * a1 + 2 ** lsb_unknown * a2 * t + u
+    u_ = a1 + t * a2 + u
 
     B = matrix(ZZ, 5, 5)
     B[0] = vector(ZZ, [K, K * 2 ** l, K * t, K * t * 2 ** l, u_])
@@ -139,8 +141,8 @@ def dsa_known_middle(n, signature1, signature2, nonce_bitsize, msb_unknown, lsb_
     x1, y1, x2, y2 = A.solve_right(vector(ZZ, b))
     assert (x1 + 2 ** l * y1 + t * x2 + 2 ** l * t * y2 + u_) % n == 0
 
-    k1 = 2 ** l * y1 + 2 ** lsb_unknown * a1 + x1
-    k2 = 2 ** l * y2 + 2 ** lsb_unknown * a2 + x2
+    k1 = partial_nonce1.sub([int(x1), int(y1)])
+    k2 = partial_nonce2.sub([int(x2), int(y2)])
     private_key1 = pow(r1, -1, n) * (s1 * k1 - h1) % n
     private_key2 = pow(r2, -1, n) * (s2 * k2 - h2) % n
     assert private_key1 == private_key2
--- test/test_hnp.py
+++ test/test_hnp.py
@@ -9,6 +9,7 @@ if sys.path[1] != path:
     sys.path.insert(1, path)
 
 from attacks.hnp import lattice_attack
+from shared.partial_integer import PartialInteger
 
 
 class TestHNP(TestCase):
@@ -25,17 +26,19 @@ class TestHNP(TestCase):
         g = 5
         x = randint(1, p - 1)
 
-        nonce_bitsize = p.bit_length()
+        nonce_bit_length = p.bit_length()
         msb_known = 7
         n_signatures = 25
         nonces = []
         signatures = []
+        partial_nonces = []
         for i in range(n_signatures):
             h, r, s, k = self._dsa(p, g, x)
             nonces.append(k)
-            signatures.append((h, r, s, k >> (nonce_bitsize - msb_known)))
+            signatures.append((h, r, s))
+            partial_nonces.append(PartialInteger.msb_of(k, nonce_bit_length, msb_known))
 
-        x_, nonces_ = next(lattice_attack.dsa_known_msb(p, signatures, nonce_bitsize, msb_known))
+        x_, nonces_ = next(lattice_attack.dsa_known_msb(p, signatures, partial_nonces))
         self.assertIsInstance(x_, int)
         self.assertIsInstance(nonces_, list)
         self.assertEqual(x, x_)
@@ -43,17 +46,19 @@ class TestHNP(TestCase):
             self.assertIsInstance(nonces_[i], int)
             self.assertEqual(nonces[i], nonces_[i])
 
-        nonce_bitsize = p.bit_length()
+        nonce_bit_length = p.bit_length()
         lsb_known = 7
         n_signatures = 25
         nonces = []
         signatures = []
+        partial_nonces = []
         for i in range(n_signatures):
             h, r, s, k = self._dsa(p, g, x)
             nonces.append(k)
-            signatures.append((h, r, s, k % (2 ** lsb_known)))
+            signatures.append((h, r, s))
+            partial_nonces.append(PartialInteger.lsb_of(k, nonce_bit_length, lsb_known))
 
-        x_, nonces_ = next(lattice_attack.dsa_known_lsb(p, signatures, nonce_bitsize, lsb_known))
+        x_, nonces_ = next(lattice_attack.dsa_known_lsb(p, signatures, partial_nonces))
         self.assertIsInstance(x_, int)
         self.assertIsInstance(nonces_, list)
         self.assertEqual(x, x_)
@@ -61,15 +66,18 @@ class TestHNP(TestCase):
             self.assertIsInstance(nonces_[i], int)
             self.assertEqual(nonces[i], nonces_[i])
 
-        nonce_bitsize = p.bit_length()
-        msb_unknown = 10
+        nonce_bit_length = p.bit_length()
         lsb_unknown = 20
+        msb_unknown = 10
+        middle_known = nonce_bit_length - lsb_unknown - msb_unknown
         h1, r1, s1, k1 = self._dsa(p, g, x)
-        signature1 = (h1, r1, s1, (k1 >> lsb_unknown) % (2 ** (nonce_bitsize - msb_unknown)))
+        signature1 = (h1, r1, s1)
+        partial_nonce1 = PartialInteger.middle_of(k1, middle_known, lsb_unknown, msb_unknown)
         h2, r2, s2, k2 = self._dsa(p, g, x)
-        signature2 = (h2, r2, s2, (k2 >> lsb_unknown) % (2 ** (nonce_bitsize - msb_unknown)))
+        signature2 = (h2, r2, s2)
+        partial_nonce2 = PartialInteger.middle_of(k2, middle_known, lsb_unknown, msb_unknown)
 
-        x_, k1_, k2_ = lattice_attack.dsa_known_middle(p, signature1, signature2, nonce_bitsize, msb_unknown, lsb_unknown)
+        x_, k1_, k2_ = lattice_attack.dsa_known_middle(p, signature1, partial_nonce1, signature2, partial_nonce2)
         self.assertIsInstance(x_, int)
         self.assertIsInstance(k1_, int)
         self.assertIsInstance(k2_, int)
