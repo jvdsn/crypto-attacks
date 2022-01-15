668b1b5c9b7f3a7c6b0b4f0253bf3f4e511fd1fc
Joachim Vandersmissen <git@jvdsn.com>
2022-01-15 21:38:24 +0100

    Added modular trivariate roots from Blomer-May.

--- shared/small_roots/blomer_may.py
+++ shared/small_roots/blomer_may.py
@@ -3,6 +3,45 @@ import logging
 from shared import small_roots
 
 
+def modular_trivariate(f, N, m, t, X, Y, Z, roots_method="groebner"):
+    """
+    Computes small modular roots of a trivariate polynomial.
+    More information: Blomer J., May A., "New Partial Key Exposure Attacks on RSA" (Section 4)
+    :param f: the polynomial
+    :param N: the modulus
+    :param m: the parameter m
+    :param t: the parameter t
+    :param Y: an approximate bound on the y roots
+    :param Z: an approximate bound on the z roots
+    :param roots_method: the method to use to find roots (default: "groebner")
+    :return: a generator generating small roots (tuples of y and z roots) of the polynomial
+    """
+    pr = f.parent()
+    x, y, z = pr.gens()
+
+    logging.debug("Generating shifts...")
+
+    shifts = []
+    monomials = set()
+    for i in range(m + 1):
+        for j in range(i + 1):
+            for k in range(j + 1):
+                g = x ** (j - k) * z ** k * N ** i * f ** (m - i)
+                shifts.append(g)
+                monomials.update(g.monomials())
+
+            for k in range(1, t + 1):
+                h = x ** j * y ** k * N ** i * f ** (m - i)
+                shifts.append(h)
+                monomials.update(h.monomials())
+
+    L = small_roots.fill_lattice(shifts, monomials, [X, Y, Z])
+    L = small_roots.reduce(L)
+    polynomials = small_roots.reconstruct_polynomials(L, f, monomials, [X, Y, Z])
+    for roots in small_roots.find_roots(polynomials, pr, method=roots_method):
+        yield roots[x], roots[y], roots[z]
+
+
 def modular_bivariate(f, e, M, m, t, Y, Z, roots_method="groebner"):
     """
     Computes small modular roots of a bivariate polynomial.
