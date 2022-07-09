22e246c734403876228ab4f7316eb78bd10f7a0f
Joachim Vandersmissen <git@jvdsn.com>
2022-07-09 23:59:01 +0200

    PARI method is unreliable with non-zero invariants

--- shared/complex_multiplication.py
+++ shared/complex_multiplication.py
@@ -10,32 +10,13 @@ from sage.all import pari
 def hilbert_class_polynomial_roots(D, q):
     """
     Computes the roots of H_D(X) mod q given D and q.
-    When the bit length of D is 18 or less, the Sage built-in function hilbert_class_polynomial is used.
-    Otherwise, PARI is used with the best possible class invariant.
     TODO: implement "Accelerating the CM method" by Sutherland.
     :param D: the CM discriminant (negative)
     :param q: the modulus q
     :return: a generator generating the roots (values j)
     """
     assert D < 0 and (D % 4 == 0 or D % 4 == 1)
-    if D.bit_length() <= 18:
-        H = hilbert_class_polynomial(D)
-    else:
-        inv = 0
-        if D % 8 == 1 and (D % 3 == 1 or D % 3 == 2):
-            inv = 1
-        elif D % 8 == 1:
-            inv = 3
-        elif D % 3 == 1 or D % 3 == 2:
-            inv = 5
-        elif D % 2 == 1 and D % 21 != 0:
-            inv = 21
-        elif D % 208 != 156:
-            inv = 26
-        elif D % 112 != 28:
-            inv = 27
-        H = pari(f"polclass({D}, {inv})")
-
+    H = hilbert_class_polynomial(D)
     pr = GF(q)["x"]
     for j in pr(H).roots(multiplicities=False):
         yield int(j)
