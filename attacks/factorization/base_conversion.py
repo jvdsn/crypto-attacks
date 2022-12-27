acf85e0ed515d366955e7ef4dbcc927768988df2
Joachim Vandersmissen <git@jvdsn.com>
2022-12-27 17:05:50 +0100

    Add base conversion factorization for 2^x bases specifically

--- attacks/factorization/base_conversion.py
+++ attacks/factorization/base_conversion.py
@@ -1,27 +1,41 @@
 import logging
 
 from sage.all import ZZ
-from sage.all import var
 
 
-def factorize(N, coefficient_threshold=50):
+def factorize(N, coefficient_threshold=32):
     """
     Recovers the prime factors from a modulus by converting it to different bases.
     :param N: the modulus
     :param coefficient_threshold: the threshold of coefficients below which we will try to factor a base k polynomial
     :return: a tuple containing the prime factors
     """
-    x = var("x")
+    R = ZZ["x"]
     base = 2
     while True:
         logging.debug(f"Trying base {base}...")
-
-        polynomial = 0
-        for i, e in enumerate(ZZ(N).digits(base)):
-            polynomial += e * x ** i
-
-        logging.debug(f"Got {len(polynomial.coefficients())} coefficients")
-        if len(polynomial.coefficients()) < coefficient_threshold:
-            return tuple(map(lambda f: int(f[0].subs(x=base)), polynomial.factor_list()))
+        poly = R(ZZ(N).digits(base))
+        logging.debug(f"Got {len(poly.coefficients())} coefficients")
+        if len(poly.coefficients()) < coefficient_threshold:
+            facs = poly.factor()
+            return tuple(map(lambda f: int(f[0](base)), facs))
 
         base += 1
+
+
+def factorize_base_2x(N):
+    """
+    Recovers the prime factors from a modulus by converting it to different bases of the form 2^x.
+    :param N: the modulus
+    :return: a tuple containing the prime factors
+    """
+    R = ZZ["x"]
+    base = 2
+    while True:
+        logging.debug(f"Trying base {base}...")
+        poly = R(ZZ(N).digits(base))
+        facs = poly.factor()
+        if len(facs) > 1:
+            return tuple(map(lambda f: int(f[0](base)), facs))
+
+        base *= 2
